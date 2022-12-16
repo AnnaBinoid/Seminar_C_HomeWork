89836e53de66fff1eceb4a67158aeb95f72968d6
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-17 00:46:17 +0300

    Add Task008HW_EvenToN/ & gitignore

---
+++ Task008HW_EvenToN/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Add your positive integer, please!");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+if (number < 0)
+{
+    Console.WriteLine($"The pozitive number must be greater than zero. {number} is not positiv. Try again!");
+}
+
+else
+{
+
+    while (count + 1 < number)
+    {
+        count = count + 2;
+        Console.Write($"{count}, ");
+
+    }
+
+}

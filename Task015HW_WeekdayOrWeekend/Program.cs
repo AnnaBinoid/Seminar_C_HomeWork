ed3bc36db0ae95477f272c41436f14a71f285831
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-20 21:21:04 +0300

    Add Task015HW_WeekdayOrWeekend/

---
+++ Task015HW_WeekdayOrWeekend/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+15. Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот 
+день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Please, enter the number from 1 till 7 : ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 8 && userNumber > 0)
+{
+    if (userNumber > 0 && userNumber < 6)
+    {
+        Console.WriteLine("No. (Poor you, it's a weekday.)");
+    }
+    else Console.WriteLine("Yes. (Lucky you! It' s a weekend! Have fun!");
+}
+else Console.WriteLine($"Number {userNumber} is not from 1 till 7. Try again!");
+
+

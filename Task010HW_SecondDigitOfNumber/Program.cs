ad2d5fff38f5451c6815e0d9069dcf2add7e05af
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-20 20:30:36 +0300

    Add Task010HW_SecondDigitOfNumber

---
+++ Task010HW_SecondDigitOfNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+10. Напшите программу, которая принимает на вход трехзначное число
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+УТОЧНЕНИЕ: НЕ БЫЛО СКАЗАНО, ЧТО ТРЕХЗНАЧНОЕ ЧИСЛО ДОЛЖНО БЫТЬ 
+НАТУРАЛЬНЫМ. ДОБАВИЛА ПРОВЕРКУ НА ОТРИЦАТЕЛЬНЫЕ И ВЫВОЖУ МОДУЛЬ
+ВТОРОГО ЧИСЛА.
+*/
+Console.Write("Enter three-digit number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number > 99 && number < 1000) || (number < -99 && number > -1000))
+{
+    Console.WriteLine($"Your number is {number}.");
+
+    int secondDigitOfOurNumber = SecondDigitOfNumber(number);
+    Console.WriteLine($"Second digit of number {number} is {secondDigitOfOurNumber}.");
+
+    int SecondDigitOfNumber(int ourNumber)
+    {
+        int secondDigit = Math.Abs((ourNumber % 100 - ourNumber % 10) / 10);
+        return secondDigit;
+    }
+}
+else Console.WriteLine($"{number} is not three-digit number. Try again!");

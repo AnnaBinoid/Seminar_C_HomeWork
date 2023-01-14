733369cd36d59bb7f32c2f3c33b84963f1b8810c
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-01-14 23:33:50 +0300

    Add Task027HW_SumOfNumberDigits

---
+++ Task027HW_SumOfNumberDigits/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задача 27.
+Напишите программу, которая 
+принимает на вход число и 
+выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Please, enter your number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int absUsernumber = Math.Abs(userNumber);
+
+int numberLength = NumberLength(absUsernumber);
+//Console.WriteLine($"The length of your number is {numberLength}.");
+
+int[] digits = ArrNumberDigits(absUsernumber, numberLength);
+//PrintArray(digits);
+
+int numberSum = SumNumDigits(digits);
+Console.WriteLine($"The sum of digits of your number {userNumber} is {numberSum}.");
+
+int NumberLength(int number)
+{
+    int length = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        length++;
+    }
+    return length;
+}
+
+int[] ArrNumberDigits(int numberUser, int length)
+{
+    int index = 0;
+
+    int[] numberDigits = new int[length];
+
+    
+    while (numberUser > 0)
+    {
+        numberDigits[index] = numberUser % 10;
+        numberUser = numberUser / 10;
+        index++;
+    }
+    return numberDigits;
+}
+
+int SumNumDigits (int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+/*void PrintArray (int[] printArray)
+{
+    for (int i = 0; i < printArray.Length; i++)
+    {
+        Console.Write($"{printArray[i]} ");
+    }
+}
+*/

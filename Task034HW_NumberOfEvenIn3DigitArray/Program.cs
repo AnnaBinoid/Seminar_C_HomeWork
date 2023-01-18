8726dee2648129c48d697d115c3d64fe749ff7b2
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-01-18 18:04:49 +0300

    Task034HW_NumberOfEvenIn3DigitArray

---
+++ Task034HW_NumberOfEvenIn3DigitArray/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+ 
+ [345, 897, 568, 234] -> 2
+    */
+int size = 10;
+int[] array= CreateArray(size);
+PrintArray(array);
+int count = CountOfEven(array);
+Console.WriteLine($"The number of even numbers in this array is {count}.");
+
+int[] CreateArray (int length)
+{
+    int[] arr = new int[length];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray (int[] myArray)
+{
+    for (int i = 0; i < myArray.Length-1; i++)
+    {
+        Console.Write($"{myArray[i]}, ");
+    }
+    Console.WriteLine(myArray[myArray.Length-1]);
+}
+
+int CountOfEven (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}

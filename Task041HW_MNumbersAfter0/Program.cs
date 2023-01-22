d2b5193213e4c28514d775588250fd32e291c687
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-01-22 19:42:55 +0300

    Add Task041HW_MNumbersAfter0, Task043HW_PointIntersectionOfLines

---
+++ Task041HW_MNumbersAfter0/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Please, enter the number of numbers: ");
+int numberOfNumbers = Convert.ToInt32(Console.ReadLine());
+if (numberOfNumbers > 0)
+{
+    int[] userArray = CreateAndFillArray(numberOfNumbers);
+    PrintArray(userArray);
+    int numberOfPositiveNumbers = NumberOfPositiveNumbers(userArray);
+    Console.WriteLine($"The number of positive numbers in array is {numberOfPositiveNumbers}.");
+}
+else Console.WriteLine($"Your number {numberOfNumbers} is not natural, enter another number > 0!");
+
+int[] CreateAndFillArray(int count)
+{
+    int[] array = new int[count];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Enter another number: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] printArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < printArray.Length; i++)
+    {
+        if (i < printArray.Length - 1) Console.Write($"{printArray[i]}, ");
+        else Console.WriteLine($"{printArray[i]}]");
+    }
+
+}
+
+int NumberOfPositiveNumbers(int[] thisArray)
+{
+    int count = 0;
+    for (int i = 0; i < thisArray.Length; i++)
+    {
+        if (thisArray[i] > 0) count++;
+
+    }
+    return count;
+}
---
+++ Task043HW_PointIntersectionOfLines/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.WriteLine("Enter a value for B1: ");
+double valueB1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter a value for K1: ");
+double valueK1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter a value for B2: ");
+double valueB2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter a value for K2: ");
+double valueK2 = Convert.ToDouble(Console.ReadLine());
+
+double valueX = GetX(valueB1, valueK1, valueB2, valueK2);
+double valueY = GetY(valueB1, valueK1, valueB2, valueK2, valueX);
+
+Console.WriteLine($"If B1 = {valueB1}, K1 = {valueK1}, B2 = {valueB2}, K2 = {valueK2};\nX = {valueX}; Y = {valueY}.");
+
+double GetX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double GetY(double b1, double k1, double b2, double k2, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}

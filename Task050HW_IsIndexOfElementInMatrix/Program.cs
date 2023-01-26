fac3ccffd87e727cb50379434ce2e7a393f2ee8a
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-01-26 17:43:50 +0300

    Add 47, 50, 52

---
+++ Task050HW_IsIndexOfElementInMatrix/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 50. 
+Напишите программу, 
+которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента 
+или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1,7 -> такого элемента в массиве нет
+*/
+
+Console.WriteLine("Please, enter coordinates of element!");
+Console.WriteLine("Enter the number of row: ");
+int userRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of column: ");
+int userColumn = Convert.ToInt32(Console.ReadLine());
+
+if (userRow > 0 && userColumn > 0)
+{
+    int minRandValue = 1;
+    int maxRandValue = 11;
+    int[,] matrixRand = CreateMatrixRand(minRandValue, maxRandValue);
+    PrintMatrix(matrixRand);
+
+    if (IsElementInMatrix(matrixRand, userRow, userColumn)) Console.WriteLine($"The value of element on {userRow} row and {userColumn} column is {matrixRand[userRow - 1, userColumn - 1]}.");
+    else Console.WriteLine($"There is no element with coordinates {userRow}, {userColumn}, try again!");
+}
+else Console.WriteLine($"One or both of your numbers ({userRow}, {userColumn}) are less than 0 or equal to 0. Please, try again!");
+
+int[,] CreateMatrixRand(int min, int max)
+{
+    Random rnd = new Random();
+    int rows = rnd.Next(min, max);
+    int columns = rnd.Next(min, max);
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+bool IsElementInMatrix(int[,] matrix, int row, int column)
+{
+    if (row > matrix.GetLength(0) || column > matrix.GetLength(1)) return false;
+    else return true;
+}
+
---
+++ Task052HW_ArithmeticMeanOfColumn/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Задача 52. 
+Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int matrixRows = 3;
+int matrixColumns = 4;
+int minValueRnd = 1;
+int maxValueRnd = 11;
+int[,] matrixRnd = CreateMatrix(matrixRows, matrixColumns, minValueRnd, maxValueRnd);
+PrintMatrix(matrixRnd);
+Console.WriteLine(String.Empty);
+double[] arrAverage = FindAverageInColumns(matrixRnd, matrixColumns, matrixRows);
+Console.Write("Arithmetic means of each column are: ");
+PrintArray(arrAverage);
+
+
+int[,] CreateMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(minValue, maxValue);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}   ");
+        }
+        Console.WriteLine($"]", 4);
+    }
+}
+
+double[] FindAverageInColumns(int[,] matrix, int columns, int rows)
+{
+    double[] averageInColumns = new double[columns];
+
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int summNumInColumn = 0;
+
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            summNumInColumn = (summNumInColumn + matrix[j, i]);
+        }
+        averageInColumns[i] = Math.Round(Convert.ToDouble(summNumInColumn) / Convert.ToDouble(rows), 2);
+    }
+    return averageInColumns;
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
